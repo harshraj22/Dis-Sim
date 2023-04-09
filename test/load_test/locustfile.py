70ecfaf2ed12c88a1a9e65c00f7de066ad6bb467
harshraj22 <harshraj22aug@gmail.com>
2023-04-09 18:10:13 +0530

    Improvements in the load testing script
    - Automates Auth token collection
    - Improved code and docs quality (Chat GPT)
    - Readme updated

--- test/load_test/locustfile.py
+++ test/load_test/locustfile.py
@@ -1,37 +1,53 @@
-import time
-from locust import HttpUser, task, between
+import logging
+import numpy as np
+import base64
 import requests
+from locust import HttpUser, task, between
+
+API_URL = "http://0.0.0.0:8001/submit"
+LOGIN_URL = "http://0.0.0.0:8019/login"
+USERNAME = "test"
+PASSWORD = "test"
+
+logging.basicConfig(level=logging.INFO)
 
 def get_auth_token():
-    url = 'http://0.0.0.0:8019/login'
-    data = {'username': 'test', 'password': 'test'}
-    response = requests.post(url, json=data)
+    """
+    Retrieves authentication token by sending a POST request to login API.
+
+    Returns:
+        str: Authentication token.
+    """
+    data = {'username': USERNAME, 'password': PASSWORD}
+    response = requests.post(LOGIN_URL, json=data)
     response.raise_for_status()
     auth_token = response.json().get('token')
-    print(f'Status_code: {response.status_code}, auth token: {auth_token}')
+    logging.info(f'Status_code: {response.status_code}, auth token: {auth_token}')
     return auth_token
 
-# wget https://imageio.forbes.com/specials-images/imageserve/5db4c7b464b49a0007e9dfac/Photo-of-Maltese-dog/960x0.jpg
-with open('../../artifacts/960x0.jpg', 'rb') as f:
-    img = f.read()
+img = np.random.rand(256, 256, 3)
+img = base64.b64encode(img).decode("utf-8")
 
-print(r"""curl -X POST -H "Content-Type: application/json" -d '{"username": "test", "password": "test"}' http://0.0.0.0:8019/login""")
-auth_token = get_auth_token()# input('Enter auth token: ')
+logging.info(f"curl -X POST -H 'Content-Type: application/json' -d '{{'username': '{USERNAME}', 'password': '{PASSWORD}'}}' {LOGIN_URL}")
+auth_token = get_auth_token()
 
-headers = {'User-Agent': 'Mozilla/5.0',  'Authorization': f'Bearer {auth_token}'}
-paylod = {'img1': img, 'img2': img}
+headers = {'User-Agent': 'Mozilla/5.0', 'Authorization': f'Bearer {auth_token}'}
+payload = {'img1': img, 'img2': img}
 
 def sync_health_check():
+    """
+    Sends a POST request to the API endpoint to perform a health check.
+    """
     session = requests.Session()
-    response = session.post('http://0.0.0.0:8001/submit', headers=headers, files=paylod)
-    print(f'Server responded with {response}')
-
+    response = session.post(API_URL, headers=headers, files=payload)
+    logging.info(f'Server responded with {response}')
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
     @task
     def check_api_endpoint(self):
-        self.client.post("/submit", headers=headers, files={'img1': img, 'img2': img})
-
-
+        """
+        Sends a POST request to the API endpoint using Locust client.
+        """
+        self.client.post("/submit", headers=headers, files=payload)
