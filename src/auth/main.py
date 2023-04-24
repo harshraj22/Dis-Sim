644f6dcd9943bf0888cb126fbc6a3629b146c77e
harshraj22 <harshraj22aug@gmail.com>
2023-04-24 14:25:09 +0530

    WIP: Auth using Subscription DB

--- src/auth/main.py
+++ src/auth/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from auth import AuthHandler
+import mysql.connector
 from schemas import AuthDetails
 
 
@@ -8,6 +9,14 @@ app = FastAPI()
 
 auth_handler = AuthHandler()
 users = [{'username': 'test', 'password': auth_handler.get_password_hash('test')}]
+
+database = mysql.connector.connect(
+  host ="subscription_db",
+  user ="root",
+  passwd ="mypassword",
+  database = 'DB'
+)
+cursor_object = database.cursor()
 
 @app.post('/register', status_code=201)
 def register(auth_details: AuthDetails):
@@ -24,10 +33,16 @@ def register(auth_details: AuthDetails):
 @app.post('/login')
 def login(auth_details: AuthDetails):
     user = None
-    for x in users:
-        if x['username'] == auth_details.username:
-            user = x
-            break
+    user_record = cursor_object.execute(f"""SELECT * FROM subscription_details where username = '{auth_details.username}'""")
+    user_record = cursor_object.fetchall()
+    # print(cursor_object.execute(f"""SELECT * FROM subscription_details""").fetchall())
+    
+    if len(user_record) > 0:
+        user = {
+            'username': user_record[0][0],
+            'password': user_record[0][1]
+        }
+
     
     if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
         raise HTTPException(status_code=401, detail='Invalid username and/or password')
