886a617b0440d9842372c29719dbb3543c247ea4
harshraj22 <harshraj22aug@gmail.com>
2023-04-25 12:30:28 +0530

    New updates now get synced up
    - Modified the service from short lived script to continiously running server
    - Added endpoints:
    	- Sync: to sync the changes in db corresponding to a specific user to redis
    	- Sync_all: to sync the whole db to redis
    
    Todo:
    - Hit sync_all endpoint at the start of the project (to be used for load_testing)

--- src/data_population/populate.py
+++ src/data_population/populate.py
@@ -2,6 +2,12 @@
 import redis
 import mysql.connector
 import logging
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Username(BaseModel):
+  username: str
 
 
 logger = logging.getLogger(__name__)
@@ -15,22 +21,38 @@ database = mysql.connector.connect(
   database = 'DB'
 )
 
+app = FastAPI()
+
 r = redis.StrictRedis(host='redis', port=6379, decode_responses=True, password="")
 ALLOWED_PREFIX = 'allowed' # max number of requests allowed per WINDOW_LENGTH
 
 # preparing a cursor object
 cursor_object = database.cursor()
-user_record = """SELECT * FROM subscription_details """
-records = cursor_object.execute(user_record)
-result = cursor_object.fetchall()
 
-# >>> print(result)
-# [('free_user', 'Free', 10, 1), ('basic_user', 'Basic', 100, 15), ('advanced_user', 'Advanced', 1000, 60), ('test', 'Advanced', 1000, 60)]
+@app.post('/sync', status_code=200)
+def sync(username: Username):
+  print(f'Syncing {username} to redis')
+  user_record = f"""SELECT * FROM subscription_details where username = '{username.username}'"""
+  print(user_record)
+  cursor_object.execute(user_record)
+  result = cursor_object.fetchall()
 
-logger.debug(result)
-database.close()
+  for username, subscription_tier, request_limit, retention_period in result:
+    r.set(f'{ALLOWED_PREFIX}-{username}', request_limit) 
 
-for username, subscription_tier, request_limit, retention_period in result:
-  r.set(f'{ALLOWED_PREFIX}-{username}', request_limit)
 
-r.close()
+@app.get('/sync_all', status_code=200)
+def sync_all():
+  user_record = """SELECT * FROM subscription_details """
+  records = cursor_object.execute(user_record)
+  result = cursor_object.fetchall()
+
+  # >>> print(result)
+  # [('free_user', 'Free', 10, 1), ('basic_user', 'Basic', 100, 15), ('advanced_user', 'Advanced', 1000, 60), ('test', 'Advanced', 1000, 60)]
+
+  logger.debug(result)
+
+  for username, subscription_tier, request_limit, retention_period in result:
+    r.set(f'{ALLOWED_PREFIX}-{username}', request_limit)
+
+# r.close()
