95c8b9b093026edb1310fc6d4ce8ce3c0c80ed08
harshraj22 <harshraj22aug@gmail.com>
2023-05-03 12:35:30 +0530

    Check Rate Limiter as well for Free Account

--- test/integration_test/main.py
+++ test/integration_test/main.py
@@ -9,7 +9,7 @@ class TestImageSimilarityWorkflow(unittest.TestCase):
 
     def setUp(self):
         # Authenticate and store the auth token for later use
-        r = requests.post('http://auth:8019/login', json={'username': 'test', 'password': 'test'})
+        r = requests.post('http://auth:8019/login', json={'username': 'free_user', 'password': 'free'})
         self.assertEqual(r.status_code, 200)
         self.token = r.json()['token']
 
@@ -40,6 +40,25 @@ class TestImageSimilarityWorkflow(unittest.TestCase):
         r = requests.get(f'http://api:8001/result/{task_id}')
         logging.info(f'{r.status_code} {r.json()}')
         self.assertEqual(r.status_code, 200)
+
+    def test_rate_limit(self):
+        # A User Belonging to Free Tier can submit 5 images per minute
+        token = self.token
+
+        # Submit images 9 times
+        headers = {"accept": "application/json", 'Authorization': f'Bearer {token}'}
+        with open('image.jpg', 'rb') as f:
+            img = f.read()
+        files = {"img1": img, "img2": img}
+        for _ in range(9):
+            r = requests.post('http://api:8001/submit', headers=headers, files=files)
+            self.assertEqual(r.status_code, 200)
+            task_id = r.json()
+            logging.info(f'Task id: {task_id}')
+
+        # Submit images one more time, which should result in a 403 error
+        r = requests.post('http://api:8001/submit', headers=headers, files=files)
+        self.assertEqual(r.status_code, 403)
 
 if __name__ == '__main__':
     unittest.main()
