f2cb153a753ecc7572b190ec994211c004b9f732
harshraj22 <harshraj22aug@gmail.com>
2023-05-15 14:53:22 +0530

    Fixes the issue of new user registration
    - On registering new users, the redis was not being updated even after hitting the `sync` endpoint
    - probably the open connection was not able to read the changes made in db
    - a working fix for now is to create and destroy connections each time the endpoint is hit
    - will come up with better fixes using sql triggers in future

--- src/data_population/populate.py
+++ src/data_population/populate.py
@@ -15,26 +15,27 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
   
 
-database = mysql.connector.connect(
-  host ="subscription_db",
-  user ="root",
-  passwd ="mypassword",
-  database = 'DB'
-)
-
 app = FastAPI()
 
 r = redis.StrictRedis(host='redis', port=6379, decode_responses=True, password="")
 ALLOWED_PREFIX = 'allowed' # max number of requests allowed per WINDOW_LENGTH
 
-# preparing a cursor object
-cursor_object = database.cursor()
 
 @app.post('/sync', status_code=200)
 def sync(username: Username):
-  logger.info(f'Syncing {username} to redis')
+  database = mysql.connector.connect(
+    host ="subscription_db",
+    user ="root",
+    passwd ="mypassword",
+    database = 'DB'
+  )
+
+  # preparing a cursor object
+  cursor_object = database.cursor()
+
+  print(f'Syncing {username} to redis')
   user_record = f"""SELECT * FROM subscription_details where username = '{username.username}'"""
-  logger.info('User Record:', user_record)
+  print('User Record:', user_record)
   cursor_object.execute(user_record)
   result = cursor_object.fetchall()
 
@@ -42,9 +43,23 @@ def sync(username: Username):
     r.set(f'{ALLOWED_PREFIX}-{username}', request_limit) 
 
 
+  cursor_object.close()
+  database.close()
+
 @app.on_event("startup")
 @app.get('/sync_all', status_code=200)
 def sync_all():
+
+  database = mysql.connector.connect(
+    host ="subscription_db",
+    user ="root",
+    passwd ="mypassword",
+    database = 'DB'
+  )
+
+  # preparing a cursor object
+  cursor_object = database.cursor()
+
   user_record = """SELECT * FROM subscription_details """
   records = cursor_object.execute(user_record)
   result = cursor_object.fetchall()
@@ -56,6 +71,9 @@ def sync_all():
 
   for username, subscription_tier, request_limit, retention_period in result:
     r.set(f'{ALLOWED_PREFIX}-{username}', request_limit)
+
+  cursor_object.close()
+  database.close()
 
 @app.get('/healthcheck', status_code=200)
 def healthcheck():
